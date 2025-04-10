a69cc72458f9e64f77d444c10c4c5f75ba502c2b
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-04-10 09:22:36 -0300

    update

--- Exercise 4.py
+++ Exercise 4.py
@@ -1,6 +1,9 @@
 num = int(input("Digite um número inteiro maior que 0: "))
-if num <= 0:
-    print("Numero inválido...")
+if num == 0:
+    print("Numero invalido...")
+if num < 0:
+    for i in range(num,0):
+        print(i, end=" ")
 else:
     for i in range(1,num+1):
-        print(f"{i:02d}", end=" ")
+        print(i, end=" ")
