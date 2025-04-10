3b25f8d75a724065c34db5d8df7caf639b296ad9
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-04-10 11:21:01 -0300

    update do ultimo exercicio para pedir a quantidade de média.

--- Exercise 7.py
+++ Exercise 7.py
@@ -1,6 +1,7 @@
+valor = int(input("Digite a quantidade de números que você quer colocar: "))
 soma = 0
-for i in range(5):
+for i in range(valor):
     num = int(input("Digite um número: "))
     soma += num
-media = soma/5
-print(f"A média desses números é {media}!")
+media = soma/valor
+print(f"A média desses números é {media:.2f}!")
