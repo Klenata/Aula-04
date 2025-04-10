41d8e93debf8cf6f1cdb63b0af73bf4906040632
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-04-10 10:55:12 -0300

    update

--- Exercise 6.py
+++ Exercise 6.py
@@ -1,10 +1,9 @@
 numero = 0
-numero2 = 0
-for i in range(10):
+quantidade = 10
+for i in range(quantidade):
     num = int(input("Digite um número: "))
     if 10 <= num <= 20:
         numero +=1
-    else:
-        numero2 +=1
+numero2 = quantidade - numero
 print(" ")
 print(f"Você digitou {numero} números entre 10 e 20 e\n{numero2} números fora deste intervalo!")
